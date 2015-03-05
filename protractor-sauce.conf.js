3cd606cb7c9b1a7c19c1a9cc1d7cfb40a92bce3a
Devin Andrews <devinandrews@Devins-MacBook-Pro.local>
2015-03-05 15:39:26 -0500

    Sauce labs

--- protractor-sauce.conf.js
+++ protractor-sauce.conf.js
@@ -3,12 +3,12 @@ exports.config = {
 
     // If sauceUser and sauceKey are specified, seleniumServerJar will be ignored.
     // The tests will be run remotely using SauceLabs.
-    sauceUser: 'user',
-    sauceKey: 'key',
+    sauceUser: 'jd_devinandrews',
+    sauceKey: 'f8d04cf9-2d2c-4b35-bbcd-940f8da479c6',
 
     // Spec patterns are relative to the current working directly when
     // protractor is called.
-    specs: ['specs/*.js'],
+    specs: ['specs/nonAngularSampleSpec.js'],
 
     // Options to be passed to Jasmine-node.
     jasmineNodeOpts: {
@@ -20,6 +20,10 @@ exports.config = {
         // If you need to interact with a non-Angular page, you may access the wrapped webdriver instance
         // directly with browser.driver. This is a an alias.
         global.dv = browser.driver;
+
+        global.isAngularSite = function(flag){
+            browser.ignoreSynchronization = !flag;
+        };
     }
 
 };
